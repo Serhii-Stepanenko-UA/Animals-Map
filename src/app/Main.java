22db90ba3707ca5f4b9b21dacff016915e1f506c
Pulsar UA <sergivstep@gmail.com>
2024-07-24 00:00:22 +0300

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new UIOperator().getOutput(
+                new DataHandler().getById("cat55"));
+    }
+}
